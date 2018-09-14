60e1ec1c3a259ab308649eea413df0b03c6ea8e9
Juan Luis <juan@mphclub.com>
2018-09-14 15:19:14 -0400

    sign in added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import SignIn from './components/SignIn/SignIn';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <SignIn></SignIn>
       </div>
     );
   }

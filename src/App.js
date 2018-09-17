6d2668620a236749ce4623dfa8efed5a3eb2383e
Daniel Cardenas <dan85.cardenas@gmail.com>
2018-09-17 09:27:50 -0400

    Initial page load design.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import SignIn from './components/SignIn/SignIn';
+// import SignIn from './components/SignIn/SignIn';
+import InitialPage from './components/InitialPage/InitialPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SignIn></SignIn>
+        {/* <SignIn></SignIn> */}
+        <InitialPage />
       </div>
     );
   }
---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+
+export const StyledAppBar = styled(AppBar)`
+    box-shadow: none !important;
+    background-color: black !important;
+`
+
+export const StyledButton = styled(Button)`
+    text-transform: none !important;
+`

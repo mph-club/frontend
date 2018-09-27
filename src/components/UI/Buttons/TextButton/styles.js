02e40f4e92283934d6b414106c8d2aca59af83d7
Daniel Cardenas <dan85.cardenas@gmail.com>
2018-09-27 09:13:24 -0400

    Added TextButton component.

---
+++ src/components/UI/Buttons/TextButton/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { palette } from '../../../../theme';
+
+export const StyledTextButton = styled.button`
+    && {
+        cursor: pointer;
+        padding: 0;
+        background-color: transparent;
+        background-image: none;
+        border: 1px solid transparent;
+        white-space: nowrap;
+        font-size: 1rem;
+        font-weight: 600;
+        color: ${palette.blue};
+    }
+`
+

0377f1aff410aa1cd13c5515cf8e5dbbc5511f1f
Daniel Cardenas <dan85.cardenas@gmail.com>
2018-10-01 15:06:27 -0400

    Worked on checkout styles.

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -7,7 +7,10 @@ import RateStars from '../RateStars/RateStars';
 import PrimaryButton from '../UI/Buttons/PrimaryButton/PrimaryButton';
 import InfoIcon from '@material-ui/icons/InfoOutlined';
 import Footer from '../Navigation/Footer/Footer';
-import { space } from '../../theme';
+import { space, palette } from '../../theme';
+import Title from '../../components/UI/CustomTypography/Title/Title';
+import CheckIcon from '../Icons/CheckIcon';
+import TextButton from '../UI/Buttons/TextButton/TextButton';
 
 import {
     StyledGridContainer,
@@ -16,8 +19,11 @@ import {
     StyledStepsContainer,
     StyledRowContainer,
     StyledRightContainer,
-    StyledWhiteContainer
+    StyledWhiteContainer,
+    StyledButtonWrapper,
+    StyledLink
 } from './styles';
+import { FormHelperText } from '@material-ui/core';
 
 const Checkout = () => {
     return (
@@ -25,9 +31,9 @@ const Checkout = () => {
             <StyledGridContainer>
                 <Grid container spacing={40}>
                     <Grid item xs={12}>
-                        <Typography 
-                            variant="display2" 
-                            color="primary" 
+                        <Typography
+                            variant="display2"
+                            color="primary"
                             component="h1"
                         >
                             Checkout
@@ -35,9 +41,9 @@ const Checkout = () => {
                     </Grid>
                     <Grid item xs>
                         <StyledLeftFirstContainer>
-                            <div>
+                            <div style={{ marginBottom: space[4] }}>
                                 <Typography variant="headline" component="h2" style={{ maxWidth: 320 }}>Maserati Granturismo Convertible 2016</Typography>
-                                <Typography variant="title" component="div" style={{ fontWeight: 400, marginTop: `${space[2]}`}}>$382/per day</Typography>
+                                <Typography variant="title" component="div" style={{ fontWeight: 400, marginTop: `${space[2]}` }}>$382/per day</Typography>
                                 <div style={{ marginTop: `${space[1]}` }}>
                                     <RateStars rate='4' />
                                 </div>
@@ -45,76 +51,101 @@ const Checkout = () => {
                             <SyledImage src='https://d1zgdcrdir5wgt.cloudfront.net/media/vehicle/images/ainXQzexQZeXQ4g9jM7-gg.1440x700.jpg' alt='car main' />
                         </StyledLeftFirstContainer>
                         <Divider />
-                        <div>
-                            <Typography>PICKUP AND RETURN</Typography>
-                            <Typography>Miami, Fl</Typography>
-                            <p>Exact address is provided after booking is confirmed</p>
+                        <div style={{ marginTop: `${space[4]}`, marginBottom: `${space[4]}` }}>
+                            <Title component="h2">PICKUP AND RETURN</Title>
+                            <Typography variant="title" component="p" style={{ marginTop: `${space[3]}` }}>Miami, Fl</Typography>
+                            <Typography variant="body1" component="p" style={{ color: `${palette.grey02}`, marginTop: `${space[2]}` }}>Exact address is provided after booking is confirmed</Typography>
                         </div>
                         <Divider />
-                        <div>
-                            <Typography>MILES INCLUDED</Typography>
-                            <Typography>200 mi</Typography>
-                            <p>$0.45 fee fir each additional mile</p>
+                        <div style={{ marginTop: `${space[4]}`, marginBottom: `${space[4]}` }}>
+                            <Title component="h2">MILES INCLUDED</Title>
+                            <Typography variant="title" component="p" style={{ marginTop: `${space[3]}` }}>200 mi</Typography>
+                            <Typography variant="body1" component="p" style={{ color: `${palette.grey02}`, marginTop: `${space[2]}` }}>$0.45 fee fir each additional mile</Typography>
                         </div>
                         <Divider />
-                        <div>
-                            <Typography>STEPS TO BOOK</Typography>
-                            <Typography>License information</Typography>
+                        <div style={{ marginTop: `${space[4]}`, marginBottom: `${space[4]}` }}>
+                            <Title component="h2">STEPS TO BOOK</Title>
+                            <div style={{ display: 'flex', alignItems: 'center', marginTop: `${space[3]}` }}>
+                                <CheckIcon style={{ fontSize: 16, marginRight: `${space[2]}`, color: `${palette.blue}` }} />
+                                <Typography variant="title" component="p" style={{ fontWeight: 400 }}>License information</Typography>
+                            </div>
                         </div>
                         <Divider />
                         <StyledStepsContainer>
-                            <Typography>Insurance protection</Typography>
-                            <label>Premium</label>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <CheckIcon style={{ fontSize: 16, color: `${palette.blue}`, marginRight: `${space[2]}` }} />
+                                <Typography variant="title" component="p" style={{ fontWeight: 400 }}>Insurance protection</Typography>
+                            </div>
+                            <div>
+                                <TextButton>Premium</TextButton>
+                            </div>
                         </StyledStepsContainer>
                         <Divider />
                         <StyledStepsContainer>
-                            <Typography>Payment info</Typography>
-                            <label>Visa</label>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <CheckIcon style={{ fontSize: 16, marginRight: `${space[2]}`, color: `${palette.blue}` }} />
+                                <Typography variant="title" component="p" style={{ fontWeight: 400 }}>Payment info</Typography>
+                            </div>
+                            <div>
+                                <TextButton>Visa</TextButton>
+                            </div>
                         </StyledStepsContainer>
                         <Divider />
-                        <div>
-                            <Typography>Mike L. needs to confirm this request</Typography>
-                            <p>By tapping below, you agree to pay the total shown and to the mph <a href=''>terms of service</a> and <a href=''>cancellation policy</a></p>
-                            <PrimaryButton>Request to book this car</PrimaryButton>
-                        </div>
                     </Grid>
                     <Grid item xs>
-                        <Typography>FEES AND PRICE DETAILS</Typography>
+                        <Title component="h2" style={{ marginBottom: `${space[3]}` }}>FEES AND PRICE DETAILS</Title>
                         <StyledRightContainer>
-                            <StyledRowContainer>
-                                <Typography>Trip Price</Typography>
-                                <Typography>$382.00/day</Typography>
+                            <StyledRowContainer style={{ marginLeft: `${space[3]}`, marginRight: `${space[3]}` }}>
+                                <Typography variant="body2" component="p">Trip Price</Typography>
+                                <Typography variant="body2" component="p">$382.00/day</Typography>
                             </StyledRowContainer>
-                            <StyledRowContainer>
-                                <Typography>Trip Fee<InfoIcon /></Typography>
-                                <Typography>$39.00/day</Typography>
+                            <StyledRowContainer style={{ marginLeft: `${space[3]}`, marginRight: `${space[3]}` }}>
+                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                    <Typography variant="body2" component="p" style={{ marginRight: `${space[1]}` }}>
+                                        Trip Fee
+                                    </Typography>
+                                    <InfoIcon style={{ fontSize: 16, color: `${palette.grey02}` }} />
+                                </div>
+                                <Typography variant="body2" component="p">$9.00/day</Typography>
                             </StyledRowContainer>
-                            <StyledRowContainer>
-                                <Typography>Total per day</Typography>
-                                <Typography>$382.00/day</Typography>
+                            <StyledRowContainer style={{ marginLeft: `${space[3]}`, marginRight: `${space[3]}` }}>
+                                <Typography variant="body2" component="p">Total per day</Typography>
+                                <Typography variant="body2" component="p">$382.00/day</Typography>
                             </StyledRowContainer>
                             <StyledWhiteContainer>
                                 <StyledRowContainer>
-                                    <Typography>$391.00 x 7 day trip</Typography>
-                                    <Typography>$2372.00/day</Typography>
+                                    <Typography variant="body2" component="p">$391.00 x 7 day trip</Typography>
+                                    <Typography variant="body2" component="p">$2,372.00/day</Typography>
                                 </StyledRowContainer>
                                 <StyledRowContainer>
-                                    <Typography>Trip Total</Typography>
-                                    <Typography>$2372.00/day</Typography>
+                                    <Typography variant="title" component="p">Trip Total</Typography>
+                                    <Typography variant="title" component="p">$2,372.00/day</Typography>
                                 </StyledRowContainer>
                                 <StyledRowContainer>
                                     <div>
-                                        <Typography>Refundable deposit<InfoIcon /></Typography>
-                                        <Typography>Refundable by Jul 16</Typography>
+                                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                                            <Typography variant="body2" component="p" style={{ marginRight: `${space[1]}` }}>
+                                                Refundable deposit
+                                            </Typography>
+                                            <InfoIcon style={{ fontSize: 16, color: `${palette.grey02}` }} />
+                                        </div>
+                                        <Typography variant="body1" component="p" style={{ color: `${palette.grey02}`, marginTop: `${space[1]}` }}>Refundable by Jul 16</Typography>
                                     </div>
-                                    <Typography>$382.00/day</Typography>
+                                    <Typography variant="body2" component="p">$1,500.00</Typography>
                                 </StyledRowContainer>
                             </StyledWhiteContainer>
                         </StyledRightContainer>
                     </Grid>
+                    <StyledButtonWrapper>
+                        <div style={{marginBottom: `${space[4]}` }}>
+                            <Typography variant="body1" component="p" style={{ fontWeight: 600 }}>Mike L. needs to confirm this request</Typography>
+                            <Typography variant="body1" component="p" style={{ marginTop: `${space[3]}` }}>By clicking below, you agree to pay the total shown and to the mph <StyledLink href=''>terms of service</StyledLink> and <StyledLink href=''>cancellation policy</StyledLink></Typography>
+                        </div>
+                        <PrimaryButton>Request to book this car</PrimaryButton>
+                    </StyledButtonWrapper>
                 </Grid>
             </StyledGridContainer>
-            <Footer/>
+            <Footer />
         </Aux>
     );
 }
--- src/components/Checkout/styles.js
+++ src/components/Checkout/styles.js
@@ -1,22 +1,5 @@
 import styled from 'styled-components';
-import { space } from '../../theme';
-
-export const StyledContainer = styled.div`
-
-`
-// export const StyledGridContainer = styled.div`
-//     width: 100%;
-//     display: flex;
-//     flex-wrap: wrap;
-//     box-sizing: border-box;
-//     padding-left: 0;
-//     padding-right: 0;
-//     margin: 32px auto;
-
-//     @media (min-width: 768px) {
-//         width: 80%;
-//     }
-// `
+import { space, palette } from '../../theme';
 
 export const StyledGridContainer = styled.div`
     max-width: 960px;
@@ -48,19 +31,22 @@ export const SyledImage = styled.img`
     float: rigth;
     width: 120px;
     height: 80px;
+    border-radius: 8px;
 `
 
+
 export const StyledStepsContainer = styled.div`
-    justify-content: space-between;
-    margin: 32px 0;
     display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: ${space[4]};
+    margin-bottom: ${space[4]};
 `
 
 export const StyledRightContainer = styled.div`
     border: 1px solid;
     border-radius: 10px;
     background-color: #fafafa;
-    padding: 4px 16px;
 `
 
 export const StyledRowContainer = styled.div`
@@ -70,4 +56,24 @@ export const StyledRowContainer = styled.div`
 `
 export const StyledWhiteContainer = styled.div`
     background-color: white;
+    padding: ${space[3]};
+    margin-bottom: ${space[4]};
+`
+
+export const StyledButtonWrapper = styled.div`
+    && {
+        max-width: 460px;
+        padding-left: 20px;
+        padding-right: 20px;
+        margin-top: ${space[3]};
+        margin-bottom: ${space[4]};
+    } 
+`
+
+export const StyledLink = styled.a`
+  && {
+    cursor: pointer;
+    color: ${palette.blue};
+    text-decoration: none;
+  }
 `

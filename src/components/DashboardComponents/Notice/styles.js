d571e35fd1d81041d7ee8d356d4bb2c721cd1c27
Juan Luis <juan@mphclub.com>
2018-10-03 14:45:07 -0400

    margin bottom added

--- src/components/DashboardComponents/Notice/styles.js
+++ src/components/DashboardComponents/Notice/styles.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
+import { space } from '../../../theme';
 
 export const StyledNoticeContainer = styled.div`
     border: 2px solid ${props => props.color};
     display: inline-block;
     border-radius: 14px; 
+    margin-bottom: ${space[3]}
 `
 
 export const StyleNotice = styled(Typography)`

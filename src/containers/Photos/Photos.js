0e304cf4a15108c23fc85f3ffcb2ad2bae37780b
Juan Luis <juan@mphclub.com>
2018-10-05 15:21:38 -0400

    upload images ready to style

--- src/containers/Photos/Photos.js
+++ src/containers/Photos/Photos.js
@@ -6,14 +6,43 @@ import {
     StyledExternalContainer,
     StyledFooterContainer,
     StyledCard,
-    StyledAddButton
+    StyledAddButton,
+    StyledIcon
 } from './styles';
-import tileData from '../CarsCollection/tileData';
+import IconButton from '@material-ui/core/IconButton';
+
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import { CardMedia, CardContent } from '@material-ui/core';
+import { palette } from '../../theme';
 
 class Photos extends Component {
+
+    state = {
+        imagesList: []
+    }
+
+    handleAddButton = () => {
+        if (this.state.imagesList.length <= '5') {
+            let array = [...this.state.imagesList]
+            array.push({
+                id: '123dq23',
+                img: 'https://d1zgdcrdir5wgt.cloudfront.net/media/vehicle/images/ainXQzexQZeXQ4g9jM7-gg.1440x700.jpg'
+            })
+            this.setState({
+                imagesList: array
+            })
+        }
+    }
+
+    handleDeletePhoto = (index) => {
+        let array = [...this.state.imagesList]
+            array.splice(index, 1)
+
+        this.setState({
+            imagesList: array
+        })
+    }
 
     render() {
         return (
@@ -30,14 +59,24 @@ class Photos extends Component {
                     <div style={{ marginTop: '32px' }}>
                         <Grid container spacing={40}>
                             <Grid item sm={6} md={4} lg={3}>
-                                <StyledAddButton>
-                                    <CardContent>
+                                <StyledAddButton onClick={this.handleAddButton}>
+                                    <CardContent style={{ backgroundColor: palette.grey04 }}>
+                                        <div style={{
+                                            marginTop: '20%',
+                                            justifyContent: 'center',
+                                            display: 'grid'
+                                        }}>
+                                            <Typography variant="body2"> Add Photo</Typography>
+                                            <IconButton>
+                                                <StyledIcon />
+                                            </IconButton>
+                                        </div>
 
                                     </CardContent>
                                 </StyledAddButton>
                             </Grid>
-                            {tileData.map(card => (
-                                <Grid item key={card.img} sm={6} md={4} lg={3}>
+                            {this.state.imagesList.map((card, index) => (
+                                <Grid item key={index} sm={6} md={4} lg={3}>
                                     <StyledCard>
                                         <CardMedia
                                             component="img"
@@ -46,6 +85,9 @@ class Photos extends Component {
                                             image="https://d1zgdcrdir5wgt.cloudfront.net/media/vehicle/images/totR6CBQQ1qkbq81qjCOgg.1440x700.jpg"
                                             title="Contemplative Reptile"
                                         />
+                                        <CardActions>
+                                            <Button size="small" color="primary" onClick={() => this.handleDeletePhoto(index)}>Delete</Button>
+                                        </CardActions>
                                     </StyledCard>
                                 </Grid>
                             ))}
--- src/containers/Photos/styles.js
+++ src/containers/Photos/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
-import { space } from '../../theme';
+import { space, palette } from '../../theme';
 import Card from '@material-ui/core/Card';
+import MoreIcon from '@material-ui/icons/Add';
 
 export const StyledExternalContainer = styled.div`
     max-width: 960px;
@@ -12,10 +13,16 @@ export const StyledAddButton = styled(Card)`
     display: flex;
     flex-direction: column;
     height: 100%;
+    background-color: ${palette.grey04} !important;
 
     img{
         max-width: 100%;
     }
+`
+
+export const StyledIcon = styled(MoreIcon)`
+    height: 40px;
+    width: 40px;
 `
 
 export const StyledCard = styled(Card)`

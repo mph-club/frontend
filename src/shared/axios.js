789a00475409c743a58d94cce2870f6adc2a561b
Juan Luis <juan@mphclub.com>
2018-12-19 13:47:50 -0500

    added secure http request to production enviroremnt

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL =  process.env.NODE_ENV === 'development' ? 'http://mphclub.ngrok.io/api/v1' : 'http://mphclub.biz/api/v1/';
+axios.defaults.baseURL =  process.env.NODE_ENV === 'development' ? 'http://mphclub.ngrok.io/api/v1' : 'https://mphclub.biz/api/v1/';
 
 export default axios
